a380ae4aa9b9a8195230c3a654ffa10ac7309f12
Uzayr Iqbal Hamid <hamiduzayr@gmail.com>
2025-12-17 21:00:51 +0530

    new: add MongoDB connection and user model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,33 +1,53 @@
+const User = require("../models/user.model");
 const jwtService = require("../services/jwt.service");
 
-exports.signup = (req, res) => {
 
-    const { email } = req.body;
+// signup logic (#TODO: add hashing)
+exports.signup = async (req, res) => {
 
-    // user 1 ex.
-    const user = {
-        id: "user1",
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return req.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
         email,
-    };
+        password,
+    });
 
-    const token = jwtService.generateToken(user);
+    const token = jwtService.generateToken({
+        id: user._id,
+        email: user.email,
+    });
 
-    res.json({
+    res.status(201).json({
         message: "Signup successful",
         token,
-    })
+    });
 };
 
-exports.login = (req, res) => {
+// login logic (#TODO: add hashing)
+exports.login = async (req, res) => {
 
-    const { email } = req.body;
+    const { email, password } = req.body;
 
-    const user = {
-        id: "user1",
-        email,
-    };
+    const user = await User.findOne({ email });
 
-    const token = jwtService.generateToken(user);
+    if (!user) {
+        return req.status(401).json({ message: "Invalid credentials" });
+    }
+
+    if (user.password != password) {
+        return req.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwtService.generateToken({
+        id: user._id,
+        email: user.email,
+    });
 
     res.json({
         message: "Login successful",
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+
+connectDB();
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running at http://${HOST}:${PORT}`);
